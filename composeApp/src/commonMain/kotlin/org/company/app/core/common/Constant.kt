b10d926739931401de3256f60d06a87b589063fe
Kris Wong <kris.wong@cpce-polyu.edu.hk>
2025-12-05 17:33:32 +0800

    Remove Constant.kt from tracking

--- composeApp/src/commonMain/kotlin/org/company/app/core/common/Constant.kt
+++
@@ -1,8 +0,0 @@
-package org.company.app.core.common
-
-object Constant {
-    const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
-    const val API_KEY = "AIzaSyATK5cfxRwEFXlp73Su6HrExL5_6Z0puYw"
-    const val TIMEOUT = 10000L
-    const val VIDEO_URL = "https://www.youtube.com/watch?v="
-}
